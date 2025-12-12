5580b83d8c6c966e7ab2e1f5e17be84cd5a54a10
Sabrina/423 <sassadesabrina@gmail.com>
2025-12-12 03:15:35 -0300

    babel config adicionada

--- strivo-app/babel.config.js
+++ strivo-app/babel.config.js
@@ -1,9 +1,13 @@
+// strivo-app/babel.config.js
 module.exports = function (api) {
   api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
+    ],
+    plugins: [
+      'react-native-reanimated/plugin', 
     ],
   };
 };
